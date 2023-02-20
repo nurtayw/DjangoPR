f2eddc3bc8ab8dfcffb7b66e27faab58884b2e3e
nurtaay <nurtayserik08@gmail.com>
2023-02-20 21:12:13 +0600

    Delete and Update basket with session

--- basket/basket.py
+++ basket/basket.py
@@ -17,7 +17,7 @@ class Basket():
         if product_id not in self.basket:
             self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}
 
-        self.session.modified = True
+        self.save()
 
     def __len__(self):
         return sum(item.get('qty', 0) for item in self.basket.values())
@@ -38,3 +38,20 @@ class Basket():
     def get_total_price(self):
         return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
 
+    def delete(self, product):
+        product_id = str(product)
+        if product_id in self.basket:
+            del self.basket[product_id]
+
+        self.save()
+
+    def update(self, product, qty):
+        product_id = str(product)
+
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
+
+        self.save()
+
+    def save(self):
+        self.session.modified = True
--- basket/views.py
+++ basket/views.py
@@ -22,3 +22,27 @@ def basket_add(request):
         basketqty = basket.__len__()
         response = JsonResponse({'qty': basketqty})
         return response
+
+
+def basket_delete(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
+        return response
+
+
+def basket_update(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
+        basket.update(product=product_id, qty=product_qty)
+
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
+        return response
